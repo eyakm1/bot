d6d870e2ec17034fde43a0b31807bd70f4b57c42
Eyakm <abw00001@ya.ru>
2018-05-10 23:11:43 +0300

    bugfix

--- messageHandler.py
+++ messageHandler.py
@@ -54,16 +54,15 @@ def get_answer(body):
     if distance < len(body) * 0.4:
         message, attachment = command.process()
         message = 'Я понял ваш запрос как "%s"\n\n' % key + message
-    if message:
-        return message, attachment
+    return message, attachment
 
 
 def create_answer(data, token):
     load_modules()
     user_id = data['user_id']
-    message, attachment = get_answer(data['body'].lower())
     with open(r'mysite/bad_words.txt', 'r', encoding='utf-8') as fin:
         body_words = list(map(lambda x: x.strip(), data['body'].lower().split()))
         if any(word in fin.read().split('\n') for word in body_words):
             vkapi.ban_censorship(group=159191596, user=user_id)
+    message, attachment = get_answer(data['body'].lower())
     vkapi.send_message(user_id, token, message, attachment)
